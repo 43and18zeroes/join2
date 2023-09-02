d60141eff3c4d6dd070fe73b6a8906d86ed4ee40
Christoph <c.wagner.germany@gmail.com>
2023-09-02 19:35:17 +0200

    login10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogInComponent
+    LogInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
