a7f61db7a7ba282828d3978dda6bb0134c0b1c03
Christoph <c.wagner.germany@gmail.com>
2023-09-05 18:48:01 +0200

    password mail2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthLogInComponent } from './auth-log-in/auth-log-in.component';
 import { AuthSignUpComponent } from './auth-sign-up/auth-sign-up.component';
+import { AuthPasswordMailComponent } from './auth-password-mail/auth-password-mail.component';
 
 const routes: Routes = [
   { path: '', component: AuthLogInComponent},
-  { path: 'signup', component: AuthSignUpComponent}
+  { path: 'auth-signup', component: AuthSignUpComponent},
+  { path: 'auth-password-mail', component: AuthPasswordMailComponent}
 ];
 
 @NgModule({
