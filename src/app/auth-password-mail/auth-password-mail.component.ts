7f7d941cd549c8f3f130797772659799ce54acab
Christoph <c.wagner.germany@gmail.com>
2023-09-05 18:34:53 +0200

    password mail

--- src/app/auth-password-mail/auth-password-mail.component.ts
+++ src/app/auth-password-mail/auth-password-mail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-password-mail',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthPasswordMailComponent implements OnInit {
 
+  signUpForm = new FormGroup({
+    signUpUserName: new FormControl('', Validators.required),
+    signUpEmail: new FormControl('', [Validators.required, Validators.email]),
+    signUpPassword: new FormControl('', [Validators.required, Validators.minLength(6)])
+  });
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    // Hier können Sie z.B. einen Service aufrufen, um die Daten an einen Server zu senden.
   }
 
 }
