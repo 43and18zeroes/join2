6d35b4a44125de07d40fc934780f20fcaa1ffe0c
Christoph <c.wagner.germany@gmail.com>
2023-09-06 18:58:54 +0200

    user.class.ts 2

--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -1,11 +1,11 @@
-export class User {
-    userName: string;
-    userEmailAddress: string;
-    userPassword: string;
+export class UserData {
+    private userName: string;
+    private userEmailAddress: string;
+    private userPassword: string;
 
-    constructor(obj: any) {
+    constructor(obj: UserData) {
         this.userName = obj.userName;
         this.userEmailAddress = obj.userEmailAddress;
-        this.userName = obj.userPassword;
+        this.userPassword = obj.userPassword;
     }
 }
