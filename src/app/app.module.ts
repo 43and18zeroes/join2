f9575bfbd7b9f0fabb3b36339178805af8df2601
Christoph <c.wagner.germany@gmail.com>
2023-09-06 21:11:50 +0200

    user.class.ts 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { AuthLogInComponent } from './auth-log-in/auth-log-in.component';
 import { AuthSignUpComponent } from './auth-sign-up/auth-sign-up.component';
 import { AuthPasswordMailComponent } from './auth-password-mail/auth-password-mail.component';
 import { AuthConfirmNewPasswordComponent } from './auth-confirm-new-password/auth-confirm-new-password.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 
 @NgModule({
@@ -21,7 +26,11 @@ import { AuthConfirmNewPasswordComponent } from './auth-confirm-new-password/aut
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'join2-578f8',
+    appId: '1:549281084403:web:5931b4fec0e4c6f95c5886',
+    storageBucket: 'join2-578f8.appspot.com',
+    apiKey: 'AIzaSyClzVo7zJ-ojSR1vX7z64OYk-hfMlOF-Ho',
+    authDomain: 'join2-578f8.firebaseapp.com',
+    messagingSenderId: '549281084403',
+  },
   production: false
 };
 
