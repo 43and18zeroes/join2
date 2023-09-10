2cbab27efafb0e2fa74d9a5308d3e4bfb5da0110
Christoph <c.wagner.germany@gmail.com>
2023-09-10 15:46:35 +0200

    signUpSuccess6

--- src/app/auth-password-mail/auth-password-mail.component.ts
+++ src/app/auth-password-mail/auth-password-mail.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth-password-mail',
@@ -12,13 +13,24 @@ export class AuthPasswordMailComponent implements OnInit {
     passwordMailEmail: new FormControl('', [Validators.required, Validators.email])
   });
 
-  constructor() { }
+  @ViewChild('authSuccess', { static: false }) authSuccess: ElementRef;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
-    // Hier können Sie z.B. einen Service aufrufen, um die Daten an einen Server zu senden.
+    this.authSuccessAnimation();
+    setTimeout(() => {
+      this.router.navigate(['/auth-confirm-new-password']);
+    }, 1600);
   }
 
+  authSuccessAnimation() {
+    this.authSuccess.nativeElement.classList.add('is__active');
+    setTimeout(() => {
+      this.authSuccess.nativeElement.style.display = "none";
+    }, 1450);
+  }
 }
