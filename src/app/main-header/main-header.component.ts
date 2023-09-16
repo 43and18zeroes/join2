e5f93babd1b52617cccb8dfe7b09f7ff69e7d74a
Christoph <c.wagner.germany@gmail.com>
2023-09-16 23:38:32 +0200

    user to collection8

--- src/app/main-header/main-header.component.ts
+++ src/app/main-header/main-header.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+// import { Firestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { getAuth, signOut } from "firebase/auth";
+// import { collection } from 'firebase/firestore';
 
 @Component({
   selector: 'app-main-header',
@@ -10,8 +13,11 @@ import { getAuth, signOut } from "firebase/auth";
 export class MainHeaderComponent implements OnInit {
 
   currentUserAuth;
+  allUserData;
+  currentUserData = [];
 
-  constructor(private router: Router) { }
+  constructor(private firestore: AngularFirestore,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.getCurrentUserAuth();
@@ -22,6 +28,7 @@ export class MainHeaderComponent implements OnInit {
     auth.onAuthStateChanged((user) => {
       if (user != null) {
         this.currentUserAuth = user;
+        this.getAllUserData();
         // console.log("user", user);
         // const displayUserName = user.email;
         // console.log("this.displayUserName", displayUserName);
@@ -29,6 +36,27 @@ export class MainHeaderComponent implements OnInit {
         // No user is signed in.
       }
     });
+  }
+
+  getAllUserData() {
+    this.firestore
+      .collection('user')
+      .valueChanges()
+      .subscribe((changes: any) => {
+        console.log("changes", changes);
+        this.allUserData = changes;
+        this.filterCurrentUserData();
+      })
+  }
+
+  filterCurrentUserData() {
+    for (const key in this.allUserData) {
+      if (this.allUserData[key].userEmailAddress === this.currentUserAuth.email) {
+        this.currentUserData.push(this.allUserData[key]);
+      }
+    }
+
+    console.log("this.currentUserData", this.currentUserData);
   }
 
   logOut() {
