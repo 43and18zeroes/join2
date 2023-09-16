e59de22660eeb1b197246978d03da2c794a68248
Christoph <c.wagner.germany@gmail.com>
2023-09-17 00:07:37 +0200

    user to collection11

--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -1,19 +1,19 @@
 export class User {
     public userName: string;
     public userEmailAddress: string;
-    public userPassword: string;
+    // public userPassword: string;
 
     constructor() {
         this.userName = this.userName;
         this.userEmailAddress = this.userEmailAddress;
-        this.userPassword = this.userPassword;
+        // this.userPassword = this.userPassword;
     }
 
     public toJSON() {
         return {
             userName: this.userName,
             userEmailAddress: this.userEmailAddress,
-            userPassword: this.userPassword
+            // userPassword: this.userPassword
         }
     }
 }
