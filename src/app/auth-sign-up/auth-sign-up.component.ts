248da779f4e200809b3fa67e6da884af6e4e4012
Christoph <c.wagner.germany@gmail.com>
2023-09-17 16:26:10 +0200

    sign up clean

--- src/app/auth-sign-up/auth-sign-up.component.ts
+++ src/app/auth-sign-up/auth-sign-up.component.ts
@@ -1,8 +1,6 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { User } from 'src/models/user.class';
-// import { Firestore, collection, collectionData } from '@angular/fire/firestore';
-// import { Observable } from 'rxjs';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
@@ -34,15 +32,7 @@ export class AuthSignUpComponent implements OnInit {
     private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    // this.user.userName = "testUser";
-    // console.log("user", this.user);
   }
-
-  // emailValidator(control: FormControl): { [key: string]: any } | null {
-  //   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-  //   const valid = emailRegex.test(control.value);
-  //   return valid ? null : { invalidEmail: true };
-  // }
 
   mailFocus() {
     this.emailAdressAlreadyExists = false;
@@ -53,7 +43,6 @@ export class AuthSignUpComponent implements OnInit {
   }
 
   signUp() {
-    // console.log(this.signUpForm.value);
     const userData = Object.assign(this.signUpForm, { userName: this.signUpForm.value.signUpUserName, email: this.signUpForm.value.signUpEmail, password: this.signUpForm.value.signUpPassword });
     this.authService.signUp(userData).then((res: any) => {
       this.isSubmitted = true;
@@ -69,8 +58,6 @@ export class AuthSignUpComponent implements OnInit {
   }
 
   createNewUserData() {
-    // this.user.userName = "testUser";
-    // console.log("user", this.user);
     this.firestore
       .collection('users')
       .add(this.user.toJSON())
