bca8653c85f8b01f6a7550eea443351c3d6e336e
Christoph <c.wagner.germany@gmail.com>
2023-09-22 23:27:10 +0200

    bu local2

--- src/app/main-header/main-header.component.ts
+++ src/app/main-header/main-header.component.ts
@@ -19,27 +19,10 @@ export class MainHeaderComponent implements OnInit {
     private userService: UserService) { }
 
   ngOnInit(): void {
-    // console.log("this.userService.allUsersData", this.userService.allUsersData)
-    // if (this.userService.allUsersData === "undefined") {
-    //   console.log("this.userService.currentUserData ist leer");
-    //   this.userService.setAllUsersDataToVar();
-    //   this.identifyCurrentUserData();
-    // } else {
-    //   this.currentUserData = this.userService.currentUserData;
-    //   console.log("header this.currentUserData", this.currentUserData);
-    // }
-
-    console.log("this.userService.allUsersData", this.userService.allUsersData)
-
     if (this.userService.allUsersData !== undefined) {
       this.currentUserData = this.userService.currentUserData[0];
-      console.log("this.currentUserData", this.currentUserData)
     } else {
-      console.log("undefined");
       this.currentUserData = JSON.parse(localStorage.getItem('currentUserData') || '[]');
-      console.log("this.currentUserData", this.currentUserData)
-      // this.userService.setAllUsersDataToVar();
-      // this.identifyCurrentUserData();
     }
   }
 
