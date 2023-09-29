d1c4c3f2a34959b2a0ac21991d8506047d205779
Christoph <c.wagner.germany@gmail.com>
2023-09-29 18:55:39 +0200

    500 width new

--- src/app/main-summary/main-summary.component.ts
+++ src/app/main-summary/main-summary.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild, AfterViewInit, HostListener  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MainComponent } from '../main/main.component';
 // import { UserService } from '../services/user-data.service';
 
@@ -7,16 +7,13 @@ import { MainComponent } from '../main/main.component';
   templateUrl: './main-summary.component.html',
   styleUrls: ['./main-summary.component.scss']
 })
-export class MainSummaryComponent implements OnInit, AfterViewInit {
+export class MainSummaryComponent implements OnInit {
 
   showGreetingScreenMobile = true;
   currenUserIsGuest = true;
   currentUserData;
   currentDate = new Date();
   futureDate = new Date(this.currentDate);
-
-  @ViewChild('toDoBoxWidth') toDoBoxWidth: ElementRef;
-  @ViewChild('urgentBoxWidth') urgentBoxWidth: ElementRef;
 
   constructor(public mainComponent: MainComponent) {
     this.futureDate.setDate(this.currentDate.getDate() + 8);
@@ -27,22 +24,6 @@ export class MainSummaryComponent implements OnInit, AfterViewInit {
     this.currentUserData = this.mainComponent.currentUserData;
     if (this.currentUserData.userName !== "Gast") {
       this.currenUserIsGuest = false;
-    }
-  }
-
-  ngAfterViewInit(): void {
-    this.matchWidths();
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: Event): void {
-    this.matchWidths();
-  }
-
-  matchWidths(): void {
-    console.log("this.toDoBoxWidth.nativeElement.offsetWidth", this.toDoBoxWidth.nativeElement.offsetWidth);
-    if (this.toDoBoxWidth && this.toDoBoxWidth.nativeElement.offsetWidth) {
-      this.urgentBoxWidth.nativeElement.style.width = `${this.toDoBoxWidth.nativeElement.offsetWidth - 50}px`;
     }
   }
 
