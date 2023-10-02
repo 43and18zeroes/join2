c38660dd1187cd7cfcb622eb204adcccd9af2e8d
Christoph <c.wagner.germany@gmail.com>
2023-10-02 18:48:36 +0200

    signUpUserNameValidator

--- src/app/shared/validators/custom-validators.ts
+++ src/app/shared/validators/custom-validators.ts
@@ -1,13 +1,13 @@
 import { FormControl } from '@angular/forms';
 
+export function signUpUserNameValidator(control: FormControl): { [key: string]: any } | null {
+    const signUpUserNameRegex = /^[a-zA-ZäöüÄÖÜ\-]+\s[a-zA-ZäöüÄÖÜ\-]+$/;
+    const valid = signUpUserNameRegex.test(control.value);
+    return valid ? null : { invalidSignUpUserNameName: true };
+}
+
 export function emailValidator(control: FormControl): { [key: string]: any } | null {
     const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     const valid = emailRegex.test(control.value);
     return valid ? null : { invalidEmail: true };
-}
-
-export function signUpUserNameValidator(control: FormControl): { [key: string]: any } | null {
-    const signUpUserNameRegex = /^(?=.{1,32}$)(.{1,31})\s+(.{1,31})$/;
-    const valid = signUpUserNameRegex.test(control.value);
-    return valid ? null : { invalidSignUpUserNameName: true };
 }
