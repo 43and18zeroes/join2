272e6caf3363614bfe97e6fc6d3f13df870232ed
Christoph <c.wagner.germany@gmail.com>
2023-10-14 13:22:20 +0200

    macontact class

---
+++ src/models/contact.class.ts
@@ -0,0 +1,9 @@
+export class Contact {
+    public contactName: string;
+    public contactFirstName: string;
+    public contactSurName: string;
+    public contactInitials: string;
+    public contactEmailAddress: string;
+    public contactPhoneNumber: string;
+    public contactColor: string;
+}
