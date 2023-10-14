141f40b163d100b0232642e7a0d21704b9a8b791
Christoph <c.wagner.germany@gmail.com>
2023-10-14 16:22:08 +0200

    contact class2

--- src/app/main-dialog-add-contact/main-dialog-add-contact.component.ts
+++ src/app/main-dialog-add-contact/main-dialog-add-contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Contact } from 'src/models/contact.class';
 
 @Component({
   selector: 'app-main-dialog-add-contact',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class MainDialogAddContactComponent {
 
+  contact = new Contact();
+
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit() {
+    
+  }
 }
--- src/models/contact.class.ts
+++ src/models/contact.class.ts
@@ -6,4 +6,26 @@ export class Contact {
     public contactEmailAddress: string;
     public contactPhoneNumber: string;
     public contactColor: string;
+
+    constructor() {
+        this.contactName = this.contactName;
+        this.contactFirstName = this.contactFirstName;
+        this.contactSurName = this.contactSurName;
+        this.contactInitials = this.contactInitials;
+        this.contactEmailAddress = this.contactEmailAddress;
+        this.contactPhoneNumber = this.contactPhoneNumber;
+        this.contactColor = this.contactColor;
+    }
+
+    public toJSON() {
+        return {
+            contactName: this.contactName,
+            contactFirstName: this.contactFirstName,
+            contactSurName: this.contactSurName,
+            contactInitials: this.contactInitials,
+            contactEmailAddress: this.contactEmailAddress,
+            contactPhoneNumber: this.contactPhoneNumber,
+            contactColor: this.contactColor
+        }
+    }
 }
