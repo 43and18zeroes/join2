4a597faf6bcf9a90f98d144bdceea86d390e537e
Christoph <c.wagner.germany@gmail.com>
2023-10-30 18:54:37 +0100

    board styling7

--- src/app/main-board/main-board.component.ts
+++ src/app/main-board/main-board.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, QueryList, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-main-board',
@@ -7,9 +7,37 @@ import { Component } from '@angular/core';
 })
 export class MainBoardComponent {
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkForHorizontalScroll();
+  }
+
+  @ViewChild('mainContainer') mainContainer: ElementRef;
+  // @ViewChild('sectionBody') sectionBody: ElementRef;
+  @ViewChildren('sectionBody') sectionBodys: QueryList<ElementRef>;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.checkForHorizontalScroll();
+  }
+
+  private checkForHorizontalScroll() {
+    const mainElement: HTMLDivElement = this.mainContainer.nativeElement;
+
+    this.sectionBodys.forEach((sectionBody: ElementRef) => {
+      const divElement: HTMLDivElement = sectionBody.nativeElement;
+      if (mainElement.scrollWidth > mainElement.clientWidth) {
+        divElement.classList.add('has-horizontal-scroll');
+        console.log("bar vorhanden")
+      }
+      else {
+        divElement.classList.remove('has-horizontal-scroll');
+        console.log("bar nicht vorhanden")
+      }
+    });
+  }
 }
