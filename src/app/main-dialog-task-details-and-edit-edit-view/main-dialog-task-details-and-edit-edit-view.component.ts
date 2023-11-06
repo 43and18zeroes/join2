0b3c34aa9de34a2ef532a63b724d207fb20b873f
Christoph <c.wagner.germany@gmail.com>
2023-11-06 17:56:30 +0100

    data transfer working3

--- src/app/main-dialog-task-details-and-edit-edit-view/main-dialog-task-details-and-edit-edit-view.component.ts
+++ src/app/main-dialog-task-details-and-edit-edit-view/main-dialog-task-details-and-edit-edit-view.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TaskDetailsCommService } from '../services/task-details-comm.service';
 import { MainDialogTaskDetailsAndEditComponent } from '../main-dialog-task-details-and-edit/main-dialog-task-details-and-edit.component';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-main-dialog-task-details-and-edit-edit-view',
@@ -10,16 +11,27 @@ import { MainDialogTaskDetailsAndEditComponent } from '../main-dialog-task-detai
 export class MainDialogTaskDetailsAndEditEditViewComponent {
 
   taskData;
+  editTaskForm = this.fb.group({
+    category: [''],
+    title: [''],
+    desciption: [''],
+    dueDate: [''],
+    priority: [''],
+    assignedTo: [[]],
+    subTasks: [],
+    subTasksCompleted: []
+  });
 
   constructor(private taskDetailsCommService: TaskDetailsCommService,
-              public mainDialogTaskDetailsAndEditComponent: MainDialogTaskDetailsAndEditComponent) { }
+    public mainDialogTaskDetailsAndEditComponent: MainDialogTaskDetailsAndEditComponent,
+    private fb: FormBuilder) { }
 
   ngOnInit() {
     this.taskData = this.mainDialogTaskDetailsAndEditComponent.taskData;
     console.log("this.taskData", this.taskData);
   }
 
-  submitChanges() {
+  onSubmit() {
     // Update changes
     // Update Successful Animation
     this.taskDetailsCommService.unsetEditMode();
