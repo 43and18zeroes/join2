060a3e2f3c2928e1f2530ff87d963dcb9af5295c
Christoph <c.wagner.germany@gmail.com>
2023-11-07 16:10:22 +0100

    delete task working3

--- src/app/main-dialog-task-details-and-edit-task-view/main-dialog-task-details-and-edit-task-view.component.ts
+++ src/app/main-dialog-task-details-and-edit-task-view/main-dialog-task-details-and-edit-task-view.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { TaskDetailsCommService } from '../services/task-details-comm.service';
 import { MainDialogTaskDetailsAndEditComponent } from '../main-dialog-task-details-and-edit/main-dialog-task-details-and-edit.component';
 import { BoardCommService } from '../services/board-comm.service';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-main-dialog-task-details-and-edit-task-view',
@@ -14,10 +15,15 @@ export class MainDialogTaskDetailsAndEditTaskViewComponent {
 
   constructor(private taskDetailsCommService: TaskDetailsCommService,
               public mainDialogTaskDetailsAndEditComponent: MainDialogTaskDetailsAndEditComponent,
-              public boardCommService: BoardCommService) {}
+              public boardCommService: BoardCommService,
+              public dialog: MatDialog) {}
 
   ngOnInit() {
     this.taskData = this.mainDialogTaskDetailsAndEditComponent.taskData;
+  }
+
+  closeDialog() {
+    this.dialog.closeAll();
   }
 
   setEditMode() {
