bfbfc1e27d75e54024d08b34fbb00685a6e45d76
Christoph <c.wagner.germany@gmail.com>
2023-11-26 13:30:34 +0100

    main ts 4

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { UserService } from '../services/user-data.service';
 import { MainCommunicationService } from '../services/main-communication.service';
 import { TaskDataService } from '../services/task-data.service';
@@ -15,10 +15,11 @@ export class MainComponent implements OnInit {
   allUsersData;
   allContactsData;
   allTasksData;
+  @ViewChild('mainSection') mainSection: ElementRef;
+
 
   currentlyDisplayed: string = 'summary';
   currentlyClicked: string = 'summary';
-
   displayMainSection(condition: 'summary' | 'board' | 'addTask' | 'contacts' | 'privacyPolicy' | 'legalNotice') {
     this.currentlyDisplayed = condition;
     this.currentlyClicked = condition;
@@ -55,7 +56,19 @@ export class MainComponent implements OnInit {
     setTimeout(() => {
       this.showGreetingScreenMobile = false;
     }, 2500);
+    this.implementAndroidHeight();
   }
 
-  
+  implementAndroidHeight() {
+    document.addEventListener('DOMContentLoaded', () => {
+      const isAndroid = /Android/i.test(navigator.userAgent);
+      if (isAndroid) {
+        this.mainSection.nativeElement.classList.add('android__height');
+        // const appMain = document.getElementsByTagName('app-main')[0];
+        // if (appMain) {
+        //   appMain.classList.add('android__height');
+        // }
+      }
+    });
+  }
 }
