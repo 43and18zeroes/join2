2cdaf80578a3352cde53bf27c8b6d5743c767097
Christoph <c.wagner.germany@gmail.com>
2023-12-15 20:26:15 +0100

    isAndroid dvh

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,15 @@ if (environment.production) {
   enableProdMode();
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const isAndroid = /Android/i.test(navigator.userAgent);
-  if (isAndroid) {
-    const appMain = document.getElementsByTagName('app-main')[0];
-    if (appMain) {
-      appMain.classList.add('android__height');
-    }
-  }
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   const isAndroid = /Android/i.test(navigator.userAgent);
+//   if (isAndroid) {
+//     const appMain = document.getElementsByTagName('app-main')[0];
+//     if (appMain) {
+//       appMain.classList.add('android__height');
+//     }
+//   }
+// });
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
