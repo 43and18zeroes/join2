f30029b9358c5405db2e0cbdf455dfa6c69407ca
Christoph <c.wagner.germany@gmail.com>
2024-10-30 14:50:09 +0100

    new approach json2

--- src/app/services/task-data.service.ts
+++ src/app/services/task-data.service.ts
@@ -11,23 +11,24 @@ export class TaskDataService {
   constructor(private firestore: AngularFirestore) { }
 
   setAllTasksDataToVarAndLocal() {
-    this.firestore
-      .collection('tasks')
-      .valueChanges()
-      .subscribe((changes: any) => {
-        this.allTasksData = changes;
-        localStorage.removeItem('allTasksData');
-        localStorage.setItem('allTasksData', JSON.stringify(changes));
-      })
+    // this.firestore
+    //   .collection('tasks')
+    //   .valueChanges()
+    //   .subscribe((changes: any) => {
+    //     this.allTasksData = changes;
+    //     localStorage.removeItem('allTasksData');
+    //     localStorage.setItem('allTasksData', JSON.stringify(changes));
+    //   })
 
       this.endpointTest();
   }
 
   async endpointTest() {
-    let result = await fetch("http://127.0.0.1:8000/kanban/");
-    console.log('result', result);
-    let resultToText = await result.text();
+    const result = await fetch("http://127.0.0.1:8000/kanban/");
+    const resultToText = await result.text();
     console.log('resultToText', resultToText);
+    // localStorage.removeItem('allTasksData');
+    // localStorage.setItem('allTasksData', JSON.stringify(resultToText));
   }
 
   getTasksDataMain() {
