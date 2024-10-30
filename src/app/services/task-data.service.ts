2a81ba65d994b08b1f0e4525090bad9cc2c4227c
Christoph <c.wagner.germany@gmail.com>
2024-10-30 15:46:59 +0100

    new approach json3

--- src/app/services/task-data.service.ts
+++ src/app/services/task-data.service.ts
@@ -2,13 +2,12 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskDataService {
-
   allTasksData;
 
-  constructor(private firestore: AngularFirestore) { }
+  constructor(private firestore: AngularFirestore) {}
 
   setAllTasksDataToVarAndLocal() {
     // this.firestore
@@ -16,24 +15,59 @@ export class TaskDataService {
     //   .valueChanges()
     //   .subscribe((changes: any) => {
     //     this.allTasksData = changes;
+    //     console.log('this.allTasksData', this.allTasksData);
     //     localStorage.removeItem('allTasksData');
     //     localStorage.setItem('allTasksData', JSON.stringify(changes));
     //   })
 
-      this.endpointTest();
+    this.endpointTest();
+  }
+
+  // convertSubTasksCompleted = (dataObject) =>
+  //   dataObject.map((task) => {
+  //     if (task.subTasksCompleted === 'True') {
+  //       task.subTasksCompleted = true;
+  //     } else if (task.subTasksCompleted === 'False') {
+  //       task.subTasksCompleted = false;
+  //     }
+  //     return task;
+  //   });
+
+  convertSubTasksCompletedToLowerCase(tasks) {
+    return tasks.map(task => {
+      if (task.subTasksCompleted) {
+        // Entferne die eckigen Klammern und teile die Zeichenkette in ein Array auf
+        let subTasksArray = task.subTasksCompleted.slice(1, -1).split(", ");
+  
+        // Konvertiere jedes Element in einen Boolean
+        subTasksArray = subTasksArray.map(value => value.toLowerCase() === "true");
+  
+        // Speichere das Array wieder im Objekt
+        task.subTasksCompleted = subTasksArray;
+      }
+      return task;
+    });
   }
 
   async endpointTest() {
-    const result = await fetch("http://127.0.0.1:8000/kanban/");
-    const resultToText = await result.text();
-    console.log('resultToText', resultToText);
-    // localStorage.removeItem('allTasksData');
-    // localStorage.setItem('allTasksData', JSON.stringify(resultToText));
+    const downloadedData = await fetch('http://127.0.0.1:8000/kanban/');
+    let dataObject = await downloadedData.json();
+    console.log('dataObject', dataObject);
+    // dataObject = this.convertSubTasksCompletedToLowerCase(dataObject);
+
+    // dataObject.subTasksCompleted = dataObject.subTasksCompleted.toLowerCase();
+
+    // console.log('dataObject', dataObject);
+    this.allTasksData = dataObject;
+    localStorage.removeItem('allTasksData');
+    localStorage.setItem('allTasksData', JSON.stringify(dataObject));
   }
 
   getTasksDataMain() {
     if (!this.allTasksData) {
-      this.allTasksData = JSON.parse(localStorage.getItem('allTasksData') || '[]');
+      this.allTasksData = JSON.parse(
+        localStorage.getItem('allTasksData') || '[]'
+      );
     }
   }
 }
