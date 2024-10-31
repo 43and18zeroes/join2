22efec45c4b580638f1788e5b61e3b9378366c53
Christoph <c.wagner.germany@gmail.com>
2024-10-31 15:10:35 +0100

    no comments

--- src/app/services/task-data.service.ts
+++ src/app/services/task-data.service.ts
@@ -23,41 +23,10 @@ export class TaskDataService {
     this.endpointTest();
   }
 
-  // convertSubTasksCompleted = (dataObject) =>
-  //   dataObject.map((task) => {
-  //     if (task.subTasksCompleted === 'True') {
-  //       task.subTasksCompleted = true;
-  //     } else if (task.subTasksCompleted === 'False') {
-  //       task.subTasksCompleted = false;
-  //     }
-  //     return task;
-  //   });
-
-  convertSubTasksCompletedToLowerCase(tasks) {
-    return tasks.map(task => {
-      if (task.subTasksCompleted) {
-        // Entferne die eckigen Klammern und teile die Zeichenkette in ein Array auf
-        let subTasksArray = task.subTasksCompleted.slice(1, -1).split(", ");
-  
-        // Konvertiere jedes Element in einen Boolean
-        subTasksArray = subTasksArray.map(value => value.toLowerCase() === "true");
-  
-        // Speichere das Array wieder im Objekt
-        task.subTasksCompleted = subTasksArray;
-      }
-      return task;
-    });
-  }
-
   async endpointTest() {
     const downloadedData = await fetch('http://127.0.0.1:8000/kanban/');
     let dataObject = await downloadedData.json();
     console.log('dataObject', dataObject);
-    // dataObject = this.convertSubTasksCompletedToLowerCase(dataObject);
-
-    // dataObject.subTasksCompleted = dataObject.subTasksCompleted.toLowerCase();
-
-    // console.log('dataObject', dataObject);
     this.allTasksData = dataObject;
     localStorage.removeItem('allTasksData');
     localStorage.setItem('allTasksData', JSON.stringify(dataObject));
