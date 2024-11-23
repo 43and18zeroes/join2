cd9454981b6c1a137c4a7591c0a6082d0d1281b4
Christoph <c.wagner.germany@gmail.com>
2024-11-23 16:11:22 +0100

    assignToggleDropdown2

--- src/app/main-dialog-add-task/main-dialog-add-task.component.ts
+++ src/app/main-dialog-add-task/main-dialog-add-task.component.ts
@@ -163,12 +163,12 @@ export class MainDialogAddTaskComponent {
   assignToggleDropdown() {
     // this.allUsersData = this.userService.allUsersData;
     this.showAssignedDropdown = !this.showAssignedDropdown;
-    for (let index = 0; index < this.selectedUsers.length; index++) {
-      const selectedUser = this.selectedUsers[index];
-      for (let index = 0; index < this.allUsersData.length; index++) {
-        const allUser = this.allUsersData[index];
-        if (selectedUser.userEmailAddress === allUser.userEmailAddress) {
-          allUser.selected = true;
+    for (let i = 0; i < this.selectedUsers.length; i++) {
+      const selectedUser = this.selectedUsers[i];
+      for (let j = 0; j < this.allUsersData.length; j++) {
+        const user = this.allUsersData[j];
+        if (selectedUser.email === user.email) {
+          user.selected = true;
         }
       }
     }
