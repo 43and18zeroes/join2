e18cc1448409e92c857324d75eca6d69345e9312
Christoph <c.wagner.germany@gmail.com>
2024-11-24 15:16:44 +0100

    toggleSubTaksk

--- src/app/main-dialog-task-details-and-edit-task-view/main-dialog-task-details-and-edit-task-view.component.ts
+++ src/app/main-dialog-task-details-and-edit-task-view/main-dialog-task-details-and-edit-task-view.component.ts
@@ -1,32 +1,33 @@
-import { Component } from '@angular/core';
-import { TaskDetailsCommService } from '../services/task-details-comm.service';
-import { MainDialogTaskDetailsAndEditComponent } from '../main-dialog-task-details-and-edit/main-dialog-task-details-and-edit.component';
-import { BoardCommService } from '../services/board-comm.service';
-import { MatDialog } from '@angular/material/dialog';
-import { UserService } from '../services/user-data.service';
-import { BackendService } from '../services/drf/backend-service.service';
+import { Component } from "@angular/core";
+import { TaskDetailsCommService } from "../services/task-details-comm.service";
+import { MainDialogTaskDetailsAndEditComponent } from "../main-dialog-task-details-and-edit/main-dialog-task-details-and-edit.component";
+import { BoardCommService } from "../services/board-comm.service";
+import { MatDialog } from "@angular/material/dialog";
+import { UserService } from "../services/user-data.service";
+import { BackendService } from "../services/drf/backend-service.service";
 
 @Component({
-  selector: 'app-main-dialog-task-details-and-edit-task-view',
-  templateUrl: './main-dialog-task-details-and-edit-task-view.component.html',
-  styleUrls: ['./main-dialog-task-details-and-edit-task-view.component.scss']
+  selector: "app-main-dialog-task-details-and-edit-task-view",
+  templateUrl: "./main-dialog-task-details-and-edit-task-view.component.html",
+  styleUrls: ["./main-dialog-task-details-and-edit-task-view.component.scss"],
 })
 export class MainDialogTaskDetailsAndEditTaskViewComponent {
-
   taskData;
   allUsersData;
   newSubTasksCompleted;
 
-  constructor(private taskDetailsCommService: TaskDetailsCommService,
-              public mainDialogTaskDetailsAndEditComponent: MainDialogTaskDetailsAndEditComponent,
-              public boardCommService: BoardCommService,
-              public dialog: MatDialog,
-              private backendService: BackendService,
-              private userService: UserService) { }
+  constructor(
+    private taskDetailsCommService: TaskDetailsCommService,
+    public mainDialogTaskDetailsAndEditComponent: MainDialogTaskDetailsAndEditComponent,
+    public boardCommService: BoardCommService,
+    public dialog: MatDialog,
+    private backendService: BackendService,
+    private userService: UserService
+  ) {}
 
   ngOnInit() {
     this.taskData = this.mainDialogTaskDetailsAndEditComponent.taskData;
-    console.log('this.taskData', this.taskData);
+    console.log("this.taskData", this.taskData);
     this.allUsersData = this.userService.allUsersData;
   }
 
@@ -62,9 +63,21 @@ export class MainDialogTaskDetailsAndEditTaskViewComponent {
   // }
 
   toggleSubTaksk(index: number): void {
-    this.taskData.subTasksCompleted[index] = !this.taskData.subTasksCompleted[index];
-    this.boardCommService.subTaskCompletedChange = true;
-    this.boardCommService.updatedTaskData = this.taskData;
+    // this.taskData.subTasksCompleted[index] = !this.taskData.subTasksCompleted[index];
+    // this.taskData.subtasks[index] = !this.taskData.subTasksCompleted[index];
+    const subTask = this.taskData.subtasks[index];
+    subTask.is_completed = !subTask.is_completed;
+    console.log('this.taskData.subtasks[index]', this.taskData.subtasks[index]);
+    // this.boardCommService.subTaskCompletedChange = true;
+    // this.boardCommService.updatedTaskData = this.taskData;
+    this.backendService.updateItem(this.taskData.subtasks[index], 'subtasks').subscribe(
+      (response) => {
+        console.log('User updated successfully:', response);
+      },
+      (error) => {
+        console.error('Error creating user:', error);
+      }
+    )
   }
 
   setEditMode() {
@@ -74,14 +87,14 @@ export class MainDialogTaskDetailsAndEditTaskViewComponent {
   deleteTask() {
     // this.boardCommService.taskToDelete = this.taskData;
     this.boardCommService.deleteTask();
-    this.backendService.deleteItem(this.taskData.id, 'tasks').subscribe(
+    this.backendService.deleteItem(this.taskData.id, "tasks").subscribe(
       (response) => {
-        console.log('Task deleted successfully:', response);
+        console.log("Task deleted successfully:", response);
       },
       (error) => {
-        console.error('Error deleting user:', error);
+        console.error("Error deleting user:", error);
       }
-    )
+    );
     this.closeDialog();
   }
 }
