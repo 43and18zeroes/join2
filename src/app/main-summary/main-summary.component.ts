87c335c1a24799cbb7f5fc1e5b4bb245b36ebae3
Christoph <c.wagner.germany@gmail.com>
2024-12-11 15:39:19 +0100

    getGreeting()

--- src/app/main-summary/main-summary.component.ts
+++ src/app/main-summary/main-summary.component.ts
@@ -77,4 +77,18 @@ export class MainSummaryComponent implements OnInit {
     this.mainComponent.currentlyDisplayed = 'board';
     this.mainComponent.currentlyClicked = 'board';
   }
+
+  getGreeting(): string {
+    const currentHour = new Date().getHours();
+  
+    if (currentHour >= 5 && currentHour < 12) {
+      return "Good morning!";
+    } else if (currentHour >= 12 && currentHour < 18) {
+      return "Good afternoon!";
+    } else if (currentHour >= 18 && currentHour < 22) {
+      return "Good evening!";
+    } else {
+      return "Good night!";
+    }
+  }  
 }
