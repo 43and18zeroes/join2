e91f7170bc0c074ce338ae4ac9645b1026fa0b5a
Christoph <c.wagner.germany@gmail.com>
2024-12-12 16:38:16 +0100

    comments

--- src/app/auth-sign-up/auth-sign-up.component.ts
+++ src/app/auth-sign-up/auth-sign-up.component.ts
@@ -2,11 +2,11 @@ import { USERCOLORS } from "../usercolors.constant";
 import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms";
 import { User } from "src/models/user_drf.class";
-import { BackendService } from '../services/drf/backend-service.service';
-import { BackendUserDataService } from '../services/drf/backend-user-data.service';
+import { BackendService } from "../services/drf/backend-service.service";
+import { BackendUserDataService } from "../services/drf/backend-user-data.service";
 // import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Router } from "@angular/router";
-import { AuthService } from "../services/auth.service";
+// import { AuthService } from "../services/auth.service";
 import { emailValidator, signUpUserNameValidator } from "../shared/validators/custom-validators";
 
 @Component({
@@ -33,8 +33,8 @@ export class AuthSignUpComponent implements OnInit {
   constructor(
     // private firestore: AngularFirestore,
     private router: Router,
-    private authService: AuthService,
-    private fb: FormBuilder,
+    // private authService: AuthService,
+    // private fb: FormBuilder,
     private backendService: BackendService,
     private backendUserDataService: BackendUserDataService
   ) {}
@@ -46,7 +46,6 @@ export class AuthSignUpComponent implements OnInit {
   }
 
   onSubmit() {
-    // this.signUp();
     this.getUserData();
     this.sendUserToBackend();
   }
@@ -59,6 +58,7 @@ export class AuthSignUpComponent implements OnInit {
         this.backendUserDataService.lastUserAddedId = response.id.toString();
         this.backendUserDataService.userAddedSuccessfully = true;
         this.authSuccessAnimation();
+        this.navigateHome();
       },
       (error) => {
         console.error("Error creating user:", error);
@@ -73,68 +73,7 @@ export class AuthSignUpComponent implements OnInit {
     this.user.user_color = this.generateColorFromInitials(this.user.initials);
     this.user.phone_number = null;
     this.user.type = "user_from_signup";
-    console.log("this.user", this.user);
   }
-
-  signUp() {
-    // const userData = this.getUserData();
-    // this.authService.signUp(userData).then((res: any) => {
-    //   this.populateUser(userData);
-    //   this.processSuccessfulSignup();
-    // }).catch((error: any) => {
-    //   this.handleSignupError(error);
-    // });
-  }
-
-  // populateUser(userData) {
-  //   this.user.userName = userData.userName;
-  //   this.user.userFirstName = userData.userFirstName;
-  //   this.user.userSurName = userData.userSurName;
-  //   this.user.userInitials = userData.userInitials;
-  //   this.user.userEmailAddress = userData.email;
-  //   this.user.userColor = this.generateColorFromInitials(this.user.userInitials);
-  //   this.user.userPhoneNumber = "";
-  //   this.user.type = "userSignUp";
-  // }
-
-  processSuccessfulSignup() {
-    this.isSubmitted = true;
-    this.createNewUserData();
-    this.authSuccessAnimation();
-    this.navigateHome();
-  }
-
-  handleSignupError(error: any) {
-    console.error(error);
-    this.emailAdressAlreadyExists = true;
-  }
-
-  extractUserData() {
-    const userName = this.signUpForm.value.signUpUserName;
-    const userFirstName = userName.split(" ")[0];
-    const userSurName = userName.split(" ")[1];
-    const userInitials = userFirstName.charAt(0).toUpperCase() + userSurName.charAt(0).toUpperCase();
-    const type = "userSignUp";
-
-    return { userName, userFirstName, userSurName, userInitials, type };
-  }
-
-  createUserDataObject() {
-    const { userName, userFirstName, userSurName, userInitials, type } = this.extractUserData();
-    return {
-      userName,
-      userFirstName,
-      userSurName,
-      userInitials,
-      email: this.signUpForm.value.signUpEmail,
-      password: this.signUpForm.value.signUpPassword,
-      type,
-    };
-  }
-
-  // getUserData() {
-  //   return this.createUserDataObject();
-  // }
 
   private generateColorFromInitials(initials: string): string {
     const colorIndex = this.calculateColorIndex(initials);
@@ -153,29 +92,6 @@ export class AuthSignUpComponent implements OnInit {
     }, 1600);
   }
 
-  prepareUserData() {
-    const userData = this.user.toJSON();
-    // delete userData.firebaseId;
-    return userData;
-  }
-
-  // addUserToFirestore(userData) {
-  //   this.firestore
-  //     .collection('users')
-  //     .add(userData)
-  //     .then((docRef) => {
-  //       return docRef.update({ firebaseId: docRef.id });
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error adding or updating document: ", error);
-  //     });
-  // }
-
-  createNewUserData() {
-    const userData = this.prepareUserData();
-    // this.addUserToFirestore(userData);
-  }
-
   authSuccessAnimation() {
     this.authSuccess.nativeElement.classList.add("is__active");
     setTimeout(() => {
@@ -183,3 +99,86 @@ export class AuthSignUpComponent implements OnInit {
     }, 1450);
   }
 }
+
+// signUp() {
+// const userData = this.getUserData();
+// this.authService.signUp(userData).then((res: any) => {
+//   this.populateUser(userData);
+//   this.processSuccessfulSignup();
+// }).catch((error: any) => {
+//   this.handleSignupError(error);
+// });
+// }
+
+// populateUser(userData) {
+//   this.user.userName = userData.userName;
+//   this.user.userFirstName = userData.userFirstName;
+//   this.user.userSurName = userData.userSurName;
+//   this.user.userInitials = userData.userInitials;
+//   this.user.userEmailAddress = userData.email;
+//   this.user.userColor = this.generateColorFromInitials(this.user.userInitials);
+//   this.user.userPhoneNumber = "";
+//   this.user.type = "userSignUp";
+// }
+
+// processSuccessfulSignup() {
+//   this.isSubmitted = true;
+//   this.createNewUserData();
+//   this.authSuccessAnimation();
+//   this.navigateHome();
+// }
+
+// handleSignupError(error: any) {
+//   console.error(error);
+//   this.emailAdressAlreadyExists = true;
+// }
+
+// extractUserData() {
+//   const userName = this.signUpForm.value.signUpUserName;
+//   const userFirstName = userName.split(" ")[0];
+//   const userSurName = userName.split(" ")[1];
+//   const userInitials = userFirstName.charAt(0).toUpperCase() + userSurName.charAt(0).toUpperCase();
+//   const type = "userSignUp";
+
+//   return { userName, userFirstName, userSurName, userInitials, type };
+// }
+
+// createUserDataObject() {
+//   const { userName, userFirstName, userSurName, userInitials, type } = this.extractUserData();
+//   return {
+//     userName,
+//     userFirstName,
+//     userSurName,
+//     userInitials,
+//     email: this.signUpForm.value.signUpEmail,
+//     password: this.signUpForm.value.signUpPassword,
+//     type,
+//   };
+// }
+
+// getUserData() {
+//   return this.createUserDataObject();
+// }
+
+// prepareUserData() {
+//   const userData = this.user.toJSON();
+//   // delete userData.firebaseId;
+//   return userData;
+// }
+
+// addUserToFirestore(userData) {
+//   this.firestore
+//     .collection('users')
+//     .add(userData)
+//     .then((docRef) => {
+//       return docRef.update({ firebaseId: docRef.id });
+//     })
+//     .catch((error) => {
+//       console.error("Error adding or updating document: ", error);
+//     });
+// }
+
+// createNewUserData() {
+//   const userData = this.prepareUserData();
+//   // this.addUserToFirestore(userData);
+// }
