fb0bfa7b0a9d8e54a3489dde4a1a48f4b1977256
Christoph <c.wagner.germany@gmail.com>
2024-12-22 17:15:52 +0100

    auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { AuthSignUpComponent } from './auth-sign-up/auth-sign-up.component';
 import { AuthPasswordMailComponent } from './auth-password-mail/auth-password-mail.component';
 import { AuthConfirmNewPasswordComponent } from './auth-confirm-new-password/auth-confirm-new-password.component';
 import { MainComponent } from './main/main.component';
+import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: AuthLogInComponent},
   { path: 'auth-signup', component: AuthSignUpComponent},
   { path: 'auth-password-mail', component: AuthPasswordMailComponent},
   { path: 'auth-confirm-new-password', component: AuthConfirmNewPasswordComponent},
-  { path: 'main', component: MainComponent}
+  { path: 'main', component: MainComponent, canActivate: [authGuard]}
 ];
 
 @NgModule({
